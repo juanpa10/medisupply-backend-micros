9784a1b42afb5b70d15cff0abc37b6d484c0990d
Willian Andres Piarpuzan <w.piarpuzan@uniandes.edu.co>
2025-10-31 23:09:48 -0500

    Se corrije error de BD

--- video-api/app/db.py
+++ video-api/app/db.py
@@ -1,8 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./evidence.db"
+DB_URL = os.getenv("DATABASE_URL") 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if not DB_URL:
+    host = os.getenv("DB_HOST")
+    if host:  
+        user = os.getenv("POSTGRES_USER")
+        password = os.getenv("POSTGRES_PASSWORD")
+        name = os.getenv("POSTGRES_DB")
+        port = os.getenv("DB_PORT", "5432")
+        if all([user, password, name]):
+            DB_URL = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}"
+
+if not DB_URL:
+    DB_URL = "sqlite:///evidence.db"  
+    
+engine = create_engine(DB_URL, echo=False, future=True)
+#DATABASE_URL = "sqlite:///./evidence.db"
+
+#engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
 Base = declarative_base()
